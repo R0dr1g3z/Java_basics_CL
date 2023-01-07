4df84a57b9ef273a506d35615f51ea049ccd15d2
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-07 18:51:30 +0100

    delete useless value

--- src/exceptions/Main05.java
+++ src/exceptions/Main05.java
@@ -3,8 +3,8 @@ package exceptions;
 public class Main05 {
     public static void main(String[] args) {
         try {
-            int num = Integer.parseInt("xyz");
-            int num2 = Integer.parseInt("5");
+            Integer.parseInt("xyz");
+            Integer.parseInt("5");
         } catch (NumberFormatException e) {
             System.out.println("This isn't number");
         } finally {
